de3f9ef3c4e3cf509059386343f58d1fbec255f2
Styxk1 <styxk96@gmail.com>
2023-10-11 13:27:07 +0300

    Добавил использование шрифта для вывода кириллицы

--- certificate/edit.py
+++ certificate/edit.py
@@ -9,23 +9,22 @@ def edit_cert(address):
     pdf_file = PyPDF2.PdfReader(open("certificate/start.pdf", "rb"))
     page = pdf_file.pages[0]
     text = page.extract_text()
-    str = f"{address}\n\nЗадолженность отсутствует."
+    str = f"{address}\nЗадолженность отсутствует."
     text = text.replace(old_text, str)
-    print(text)
     return create_new_cert(text, address)
 
 
 def create_new_cert(text, adr):
     pdf = FPDF()
-    text2 = text.encode("utf-8").decode("latin-1")
     lines_per_page = 4000
     text_pages = [
-        text2[i : i + lines_per_page] for i in range(0, len(text2), lines_per_page)
+        text[i : i + lines_per_page] for i in range(0, len(text), lines_per_page)
     ]
-    pdf.set_font("Times", size=14)
+    pdf.add_font("TimesNew", "", "config/timesnewromanpsmt.ttf", uni=True)
+    pdf.set_font("TimesNew", "", 14)
     for text_page in text_pages:
         pdf.add_page()
-        pdf.multi_cell(200, 10, txt=text_page, align="L")
+        pdf.multi_cell(200, 10, txt=text_page, align="C")
     t = time.localtime()
     curr_time = time.strftime("%H-%M-%S", t)
     adr = str(adr).replace(" ", "_")
